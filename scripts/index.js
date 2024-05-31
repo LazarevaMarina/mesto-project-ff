6d51cc8a6c361d8aba3bea3ab945f10a6f4b5d16
Marina Lazareva <missis.marina84@yandex.ru>
2024-06-01 00:39:06 +0500

    1 ревью

--- scripts/index.js
+++ scripts/index.js
@@ -7,3 +7,32 @@
 // @todo: Функция удаления карточки
 
 // @todo: Вывести карточки на страницу
+
+const container_main = document.querySelector('.content');
+const addButton = container_main.querySelector('.profile__add-button');
+const placesList = container_main.querySelector('.places__list');
+const cardTemplate = document.querySelector('#card-template').content;
+
+function createCard(cardTemplate, element, deleteCard) {
+    let cardElement = cardTemplate.querySelector('.card').cloneNode(true);
+    let deleteButton = cardElement.querySelector('.card__delete-button');
+
+    deleteButton.addEventListener('click', deleteCard);
+
+    cardElement.querySelector('.card__image').src = element.link;
+    cardElement.querySelector('.card__title').textContent = element.name;
+    
+    return cardElement;
+    
+};
+
+initialCards.forEach(function addCard(cardElement) {
+    const element = createCard(cardTemplate, cardElement, deleteCard);
+    placesList.append(element);
+});
+
+function deleteCard(evt) {
+    const evtTarget = evt.target.closest('.places__item').remove();
+};
+
+
