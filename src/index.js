f523e578d6dc3ad3dc226a02d73b5829b72fbe30
Marina Lazareva <missis.marina84@yandex.ru>
2024-07-17 22:22:43 +0500

    7-2

--- src/index.js
+++ src/index.js
@@ -90,7 +90,7 @@ function isValid() {
 
 // Функция, которая добавляет класс с ошибкой
 const showInputError = (element) => {
-    const formError = element.querySelector('.popup__input_type_name');
+    const formError = element.querySelector('.form__input-error-name');
 
     element.classList.add('popup__input_type_error');
     formError.classList.add('popup__error_visible');
